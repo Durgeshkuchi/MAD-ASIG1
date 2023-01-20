d68947ba1e6508b83dc17ed98ac7d6d63e04a2f1
Durgesh Kuchi <attipatladurgesh4455@gmail.com>
2023-01-20 09:49:26 -0600

    Initial Commit

---
+++ MAD-ASIG1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAD-ASIG1
+//
+//  Created by Durge K on 1/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
